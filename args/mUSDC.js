8d6df0657bd978f0e6259eb3d451072c4fa93222
jupiter0913 <jupiter256821@gmail.com>
2022-11-06 12:25:29 -0500

    Feat mock usdc contract and deploy

---
+++ args/mUSDC.js
@@ -0,0 +1,8 @@
+const MOCKUSDC_ARGS = require("../constants/mUSDC.json");
+
+module.exports = [
+  MOCKUSDC_ARGS.preMint,
+  MOCKUSDC_ARGS.interval,
+  MOCKUSDC_ARGS.faucetTotal,
+  MOCKUSDC_ARGS.faucetAmount,
+];
---
+++ contracts/MockUSDC.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./TokenFaucet.sol";
+
+contract MockUSDC is TokenFaucet {
+    constructor(
+        uint256 _premint,
+        uint256 _interval,
+        uint256 _faucetTotal,
+        uint256 _faucetAmount
+    )
+        ERC20("USD Coin.k", "USDC.k")
+        TokenFaucet(_interval, _faucetTotal, _faucetAmount)
+    {
+        _mint(msg.sender, _premint);
+    }
+}
